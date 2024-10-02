4f3a343ab7d6d064973b42309d784dc777880498
alexandre-honcord <alexandresilva@honcord.com.br>
2024-10-02 17:48:00 -0300

    Corrigindo projeto

--- server/setup/settings.py
+++ server/setup/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'intranet.urls'
+ROOT_URLCONF = 'setup.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'intranet.wsgi.application'
+WSGI_APPLICATION = 'setup.wsgi.application'
 
 
 # Database
