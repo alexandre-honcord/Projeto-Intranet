361e47bf04ded40e5b85427fe3e86b9a98f3d076
alexandre-honcord <alexandresilva@honcord.com.br>
2024-10-04 13:17:49 -0300

    Corrigindo o opportunity

--- server/quality/views.py
+++ server/quality/views.py
@@ -7,100 +7,59 @@ from rest_framework import viewsets
 from .serializers import OpportunitySerializer, NotificationSerializer
 from default.models.models_links import Tool, AppsTool
 
-class OpportunityListView(ListView):
+class BaseContextView:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tools'] = Tool.objects.all()
+        context['apps'] = AppsTool.objects.all()
+        return context
+
+class OpportunityListView(BaseContextView, ListView):
     model = Opportunity
     template_name = 'opportunity_list.html'
     context_object_name = 'opportunities'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tools'] = Tool.objects.all()
-        context['apps'] = AppsTool.objects.all()
-        return context
-
-class OpportunityDetailView(DetailView):
+class OpportunityDetailView(BaseContextView, DetailView):
     model = Opportunity
     template_name = 'opportunity_detail.html'
     context_object_name = 'opportunity'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tools'] = Tool.objects.all()
-        context['apps'] = AppsTool.objects.all()
-        return context
-
-class OpportunityCreateView(CreateView):
+class OpportunityCreateView(BaseContextView, CreateView):
     model = Opportunity
     form_class = OpportunityForm
     template_name = 'opportunity_form.html'
     success_url = reverse_lazy('quality:notification_list')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tools'] = Tool.objects.all()
-        context['apps'] = AppsTool.objects.all()
-        return context
-
-class OpportunityUpdateView(UpdateView):
+class OpportunityUpdateView(BaseContextView, UpdateView):
     model = Opportunity
     form_class = OpportunityForm
     template_name = 'opportunity_form.html'
     success_url = reverse_lazy('quality:notification_list')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tools'] = Tool.objects.all()
-        context['apps'] = AppsTool.objects.all()
-        return context
-
-class OpportunityDeleteView(DeleteView):
+class OpportunityDeleteView(BaseContextView, DeleteView):
     model = Opportunity
     template_name = 'opportunity_confirm_delete.html'
     success_url = reverse_lazy('quality:opportunity_list')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tools'] = Tool.objects.all()
-        context['apps'] = AppsTool.objects.all()
-        return context
-
 class OpportunityViewSet(viewsets.ModelViewSet):
     queryset = Opportunity.objects.all()
-    serializer_class = OpportunitySerializer
+    serializer_class = NotificationSerializer
 
-class NotificationListView(ListView):
+class NotificationListView(BaseContextView, ListView):
     model = Notification
     template_name = 'notification_list.html'
     context_object_name = 'notifications'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tools'] = Tool.objects.all()
-        context['apps'] = AppsTool.objects.all()
-        return context
-
-class NotificationDetailView(DetailView):
+class NotificationDetailView(BaseContextView, DetailView):
     model = Notification
     template_name = 'notification_detail.html'
     context_object_name = 'notification'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tools'] = Tool.objects.all()
-        context['apps'] = AppsTool.objects.all()
-        return context
-
-class NotificationCreateView(CreateView):
+class NotificationCreateView(BaseContextView, CreateView):
     model = Notification
     form_class = NotificationForm
     template_name = 'notification_form.html'
     success_url = reverse_lazy('quality:notification_list')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tools'] = Tool.objects.all()
-        context['apps'] = AppsTool.objects.all()
-        return context
 
     def form_valid(self, form):
         if form.cleaned_data['identified_by'] == '':
@@ -110,29 +69,18 @@ class NotificationCreateView(CreateView):
     def form_invalid(self, form):
         return self.render_to_response(self.get_context_data(form=form))
 
-class NotificationUpdateView(UpdateView):
+class NotificationUpdateView(BaseContextView, UpdateView):
     model = Notification
     form_class = NotificationForm
     template_name = 'notification_form.html'
     success_url = reverse_lazy('quality:notification_list')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tools'] = Tool.objects.all()
-        context['apps'] = AppsTool.objects.all()
-        return context
-
-class NotificationDeleteView(DeleteView):
+class NotificationDeleteView(BaseContextView, DeleteView):
     model = Notification
     template_name = 'notification_confirm_delete.html'
     success_url = reverse_lazy('quality:notification_list')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tools'] = Tool.objects.all()
-        context['apps'] = AppsTool.objects.all()
-        return context
-
 class NotificationViewSet(viewsets.ModelViewSet):
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
+
