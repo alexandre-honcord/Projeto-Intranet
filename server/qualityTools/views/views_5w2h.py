df9f42601b760e0ab746f886a5c5271a440f86be
augusto-honcord <augustogomes@honcord.com.br>
2024-10-08 09:40:42 -0300

    Refatorando views para incluir o contexto

--- server/qualityTools/views/views_5w2h.py
+++ server/qualityTools/views/views_5w2h.py
@@ -4,33 +4,41 @@ from rest_framework import viewsets
 from qualityTools.models.models_5w2h import Ferramenta5W2H
 from qualityTools.forms.forms_5w2h import Ferramenta5W2HForm
 from qualityTools.serializers.serializers_5w2h import Ferramenta5W2HSerializer
+from default.models.models_links import Tool, AppsTool
 
-class Ferramenta5W2HListView(ListView):
+class BaseContextView:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tools'] = Tool.objects.all()
+        context['apps'] = AppsTool.objects.all()
+        return context
+
+class Ferramenta5W2HListView(BaseContextView, ListView):
     model = Ferramenta5W2H
     template_name = 'ferramentas/ferramenta5w2h/ferramenta5w2h_list.html'
     context_object_name = 'ferramentas_5w2h'
 
 
-class Ferramenta5W2HCreateView(CreateView):
+class Ferramenta5W2HCreateView(BaseContextView, CreateView):
     model = Ferramenta5W2H
     form_class = Ferramenta5W2HForm
     template_name = 'ferramentas/ferramenta5w2h/ferramenta5w2h_form.html'
     success_url = reverse_lazy('qualityTools:qualityTools:ferramenta5w2h_list')
 
 
-class Ferramenta5W2HUpdateView(UpdateView):
+class Ferramenta5W2HUpdateView(BaseContextView, UpdateView):
     model = Ferramenta5W2H
     form_class = Ferramenta5W2HForm
     template_name = 'ferramentas/ferramenta5w2h/ferramenta5w2h_form.html'
     success_url = reverse_lazy('qualityTools:ferramenta5w2h_list')
 
 
-class Ferramenta5W2HDeleteView(DeleteView):
+class Ferramenta5W2HDeleteView(BaseContextView, DeleteView):
     model = Ferramenta5W2H
     template_name = 'ferramentas/ferramenta5w2h/ferramenta5w2h_confirm_delete.html'
     success_url = reverse_lazy('qualityTools:ferramenta5w2h_list')
 
 
-class Ferramenta5W2HViewSet(viewsets.ModelViewSet):
+class Ferramenta5W2HViewSet(BaseContextView, viewsets.ModelViewSet):
     queryset = Ferramenta5W2H.objects.all()
     serializer_class = Ferramenta5W2HSerializer
--- server/qualityTools/views/views_swot.py
+++ server/qualityTools/views/views_swot.py
@@ -4,29 +4,37 @@ from qualityTools.forms.forms_swot import FerramentaSWOTForm
 from rest_framework import viewsets
 from qualityTools.serializers.serializers_swot import FerramentaSWOTSerializer
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from default.models.models_links import Tool, AppsTool
 
-class FerramentaSWOTListView(ListView):
+class BaseContextView:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tools'] = Tool.objects.all()
+        context['apps'] = AppsTool.objects.all()
+        return context
+
+class FerramentaSWOTListView(BaseContextView, ListView):
     model = FerramentaSWOT
     template_name = 'ferramentas/ferramenta_swot/ferramenta_swot_list.html'
     context_object_name = 'ferramentas_swot'
 
-class FerramentaSWOTCreateView(CreateView):
+class FerramentaSWOTCreateView(BaseContextView, CreateView):
     model = FerramentaSWOT
     template_name = 'ferramentas/ferramenta_swot/ferramenta_swot_form.html'
     fields = ['oportunidade_melhoria', 'notificacao_evento_adverso', 'forcas', 'fraquezas', 'oportunidades', 'ameacas']
     success_url = reverse_lazy('qualityTools:ferramentaswot_list')
 
-class FerramentaSWOTUpdateView(UpdateView):
+class FerramentaSWOTUpdateView(BaseContextView, UpdateView):
     model = FerramentaSWOT
     template_name = 'ferramentas/ferramenta_swot/ferramenta_swot_form.html'
     fields = ['oportunidade_melhoria', 'notificacao_evento_adverso', 'forcas', 'fraquezas', 'oportunidades', 'ameacas']
     success_url = reverse_lazy('qualityTools:ferramentaswot_list')
 
-class FerramentaSWOTDeleteView(DeleteView):
+class FerramentaSWOTDeleteView(BaseContextView, DeleteView):
     model = FerramentaSWOT
     template_name = 'ferramentas/ferramenta_swot/ferramenta_swot_confirm_delete.html'
     success_url = reverse_lazy('qualityTools:ferramentaswot_list')
 
-class FerramentaSWOTViewSet(viewsets.ModelViewSet):
+class FerramentaSWOTViewSet(BaseContextView, viewsets.ModelViewSet):
     queryset = FerramentaSWOT.objects.all()
     serializer_class = FerramentaSWOTSerializer
