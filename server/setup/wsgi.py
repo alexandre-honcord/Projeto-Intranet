78e15de10b667ab967740d608c299f6642e5f5f1
augusto-honcord <augustogomes@honcord.com.br>
2024-10-15 14:43:13 -0300

    Refatorar wsgi.py para adicionar diretório do projeto ao sys.path

--- server/setup/wsgi.py
+++ server/setup/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for intranet project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Adiciona o diretório do projeto ao sys.path
+sys.path.append('/home/user/Projeto-Intranet/server')
+
+# Define o módulo de configurações do Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intranet.settings')
 
 application = get_wsgi_application()
